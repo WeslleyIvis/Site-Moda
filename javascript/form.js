dcc0a36209c89cef12aa9312f621b7b41a2801a5
WeslleyIvis <weslley_reive@hotmail.com>
2021-09-01 23:15:38 -0300

    mundanças no script do form

--- javascript/form.js
+++ javascript/form.js
@@ -2,41 +2,40 @@ document.title = "Cadastro"
 
 function validar() {
 
-    var nome = document.getElementById('nome')
-    var sobrenome = document.getElementById('sobrenome')
-    var email = document.getElementById('email')
-    var senha = document.getElementById('senha')
-    var telefone = document.getElementById('telefone')
-    var sexo = document.getElementById('sexo')
+    var nome = document.getElementById('nome');
+    var sobrenome = document.getElementById('sobrenome');
+    var email = document.getElementById('email');
+    var senha = document.getElementById('senha');
+    var confirmSenha = document.getElementById('confirmSenha')
+    var telefone = document.getElementById('telefone');
+    var sexo = document.getElementById('sexo');
     var newsletter = document.getElementById('newsletter').checked;
 
 
-    // verificar se o nome está vazio
-    if (nome.value == "") {
-        let newInfo = document.createElement('p')
-        let info = document.createTextNode('Campo obrigatorio!')
-        newInfo.appendChild(info);
 
-        var campo = document.getElementById('nome-erro')
-        campo.appendChild(newInfo)
+    // verificar se o nome está vazio
+
+    if (nome.value == "") {
+        nameErro.innerHTML = '<p> Campo obrigatorio!</p>'
 
         //Deixa o input com o focus
         nome.focus()
 
         //Retorna a função e não olha as outras linhas
         return;
-    }
+    } 
 
+    
     if (sobrenome.value == "") {
-        let newInfo = document.createElement('p')
-        let info = document.createTextNode('Campo obrigatorio!')
-        newInfo.appendChild(info);
 
-        var campo = document.getElementById('sobrenome-erro')
-        campo.appendChild(newInfo)
+        sobrenomeErro.innerHTML = '<p> Campo obrigatorio!</p>'
+
         sobrenome.focus()
+
         return
     }
+
+
 
     if (email.value == "") {
         let newInfo = document.createElement('p')
@@ -50,6 +49,16 @@ function validar() {
         return
     }
 
+    /*
+     _____            _           
+    /  ___|          | |          
+    \ `--.  ___ _ __ | |__   __ _ 
+     `--. \/ _ \ '_ \| '_ \ / _` |
+    /\__/ /  __/ | | | | | | (_| |
+    \____/ \___|_| |_|_| |_|\__,_|
+                                  
+    */
+
     if (senha.value == "") {
         let newInfo = document.createElement('p')
         let info = document.createTextNode('Campo obrigatorio!')
@@ -61,6 +70,52 @@ function validar() {
         senha.focus()
         return
     }
+
+    /*
+ _____              __ _                _____            _           
+/  __ \            / _(_)              /  ___|          | |          
+| /  \/ ___  _ __ | |_ _ _ __ _ __ ___ \ `--.  ___ _ __ | |__   __ _ 
+| |    / _ \| '_ \|  _| | '__| '_ ` _ \ `--. \/ _ \ '_ \| '_ \ / _` |
+| \__/\ (_) | | | | | | | |  | | | | | /\__/ /  __/ | | | | | | (_| |
+ \____/\___/|_| |_|_| |_|_|  |_| |_| |_\____/ \___|_| |_|_| |_|\__,_|
+    */
+
+
+    if (confirmSenha.value == "") {
+        let newInfo = document.createElement('p')
+        let info = document.createTextNode('A senha deve ser a mesma')
+        newInfo.appendChild(info);
+
+        var campo = document.getElementById('confirmSenha-erro');
+        campo.appendChild(newInfo);
+
+        confirmSenha.focus();
+        return
+
+    } else if (confirmSenha.value != senha) {
+
+        let newInfo = document.createElement('p')
+        let info = document.createTextNode('A senha deve ser a mesma')
+        newInfo.appendChild(info);
+
+        var campo = document.getElementById('confirmSenha-erro');
+        campo.appendChild(newInfo);
+
+        confirmSenha.focus();
+        return
+    }
+
+    /*
+ _____    _       __                 
+|_   _|  | |     / _|                
+  | | ___| | ___| |_ ___  _ __   ___ 
+  | |/ _ \ |/ _ \  _/ _ \| '_ \ / _ \
+  | |  __/ |  __/ || (_) | | | |  __/
+  \_/\___|_|\___|_| \___/|_| |_|\___|
+                                     
+    */
+
+
 
     if (telefone.value == "") {
         let newInfo = document.createElement('p')
@@ -89,5 +144,5 @@ function validar() {
         Senha: ${senha.value} <br>
         Telefone: ${telefone.value} <br>
         Sexo: ${sexo.value} </div>`)
-        
+
 }
